3a9ad5a7a781a6d9ee7ebd38f1d4fdf3281b31ef
Mihai BLANDU <mblandu@e1p68.chisinau.42.fr>
2018-07-11 19:50:22 +0300

    ok

---
+++ get_next_line.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mblandu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/11 19:47:26 by mblandu           #+#    #+#             */
+/*   Updated: 2018/01/11 20:29:46 by mblandu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char		*ft_cat(t_line *s1, char *buf, int fd)
+{
+	char	*new;
+
+	if (s1[FD].tmp == NULL)
+		new = ft_strdup(buf);
+	else
+		new = ft_strjoin(s1[FD].tmp, buf);
+	free(s1[FD].tmp);
+	return (new);
+}
+
+static int		fill(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static int		check_n(t_line *s, const int fd, char **line)
+{
+	char		*new;
+	int			i;
+
+	if (s[FD].tmp == NULL)
+		return (-2);
+	i = fill(s[FD].tmp);
+	if (i == -1)
+		return (-2);
+	else
+	{
+		s[FD].tmp[i++] = '\0';
+		free(*line);
+		*line = ft_strdup(s[FD].tmp);
+		new = ft_strdup(&s[FD].tmp[i]);
+		free(s[FD].tmp);
+		s[FD].tmp = ft_strdup(new);
+		free(new);
+		return (LINE);
+	}
+}
+
+static int		read_file(t_line *s, const int fd, char **line, char *buf)
+{
+	int		res;
+	char	*new;
+	int		i;
+
+	while (((res = read(fd, buf, BUFF_SIZE)) != 0) && res != -1)
+	{
+		i = fill(buf);
+		if (i == -1)
+			s[FD].tmp = ft_cat(s, buf, fd);
+		else
+		{
+			s[FD].tmp = ft_cat(s, buf, fd);
+			i = fill(s[FD].tmp);
+			s[FD].tmp[i++] = '\0';
+			free(*line);
+			*line = ft_strdup(s[FD].tmp);
+			new = ft_strdup(&s[FD].tmp[i]);
+			free(s[FD].tmp);
+			s[FD].tmp = ft_strdup(new);
+			free(new);
+			return (LINE);
+		}
+		ft_bzero(buf, BUFF_SIZE + 1);
+	}
+	return (res);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_line	s[4864];
+	char			buf[BUFF_SIZE + 1];
+
+	ft_bzero(buf, BUFF_SIZE + 1);
+	if (line == (void*)0)
+		return (ERROR);
+	*line = ft_strdup("\0");
+	if (fd < 0)
+		return (ERROR);
+	if ((s[FD].define = check_n(s, fd, line)) == LINE)
+		return (LINE);
+	s[FD].define = read_file(s, fd, line, buf);
+	if (s[FD].define == END && s[FD].tmp != NULL && s[FD].tmp[0] != '\0')
+	{
+		*line = ft_strdup(s[FD].tmp);
+		free(s[FD].tmp);
+		s[FD].tmp = NULL;
+		return (LINE);
+	}
+	return (s[FD].define);
+}
---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mblandu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/11 19:47:44 by mblandu           #+#    #+#             */
+/*   Updated: 2018/01/11 20:38:43 by mblandu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+# define ERROR -1
+# define END 0
+# define LINE 1
+# define FD fd % 4864
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+int					get_next_line(const int fd, char **line);
+typedef struct		s_line
+{
+	char			*tmp;
+	int				define;
+}					t_line;
+#endif
